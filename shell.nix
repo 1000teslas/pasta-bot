bf8cc8072770828303884474a45e554fe7ab387d
Kevin Tran <kevintran@airmail.cc>
2020-09-30 02:55:56 +1000

    use jdk14

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ jdk11 maven vault ];
+  buildInputs = with pkgs; [ jdk14 maven vault ];
   VAULT_ADDR = "http://127.0.0.1:8200";
 }
