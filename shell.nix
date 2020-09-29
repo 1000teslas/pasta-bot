c7cf4c5ac720e1658ec785d9b728dce98d729d58
Kevin Tran <kevintran@airmail.cc>
2020-09-30 00:35:17 +1000

    bot can send pasta

---
+++ shell.nix
@@ -0,0 +1,7 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ jdk14 maven vault ];
+  VAULT_ADDR = "http://127.0.0.1:8200";
+}
